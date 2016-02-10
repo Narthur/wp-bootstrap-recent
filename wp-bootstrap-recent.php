ba70d284abf09fc1692b14b77edc87a2b1b48b65
Nathan Arthur <narthur.a@gmail.com>
2016-02-10 15:58:50 -0600

    Correct plugin meta data

--- wp-bootstrap-recent.php
+++ wp-bootstrap-recent.php
@@ -10,18 +10,18 @@
  *
  * @link              http://example.com
  * @since             1.0.0
- * @package           Plugin_Name
+ * @package           wp-bootstrap-recent
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
- * Plugin URI:        http://example.com/plugin-name-uri/
+ * Plugin Name:       Responsive Bootstrap Recent Posts Slider
+ * Plugin URI:        https://github.com/Narthur/wp-bootstrap-recent
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            Your Name or Your Company
- * Author URI:        http://example.com/
+ * Author:            Nathan Arthur
+ * Author URI:        http://NathanArthur.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
- * Text Domain:       plugin-name
+ * Text Domain:       wp-bootstrap-recent
  * Domain Path:       /languages
  */
 
