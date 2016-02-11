9aafcc983dc2978f0c19d1b697b751c990249669
Nathan Arthur <narthur.a@gmail.com>
2016-02-11 09:45:37 -0600

    Make slider shortcode return slider content

--- wp-bootstrap-recent.php
+++ wp-bootstrap-recent.php
@@ -64,7 +64,66 @@ namespace bootstrap_slider_space {
 	add_action( 'wp_enqueue_scripts', 'bootstrap_slider_space\\enqueue_assets' );
 
 	function slider( $atts ) {
-		return '<p>I\'m a slider</p>';
+		$a = shortcode_atts( array(
+			'quantity' => 3
+		), $atts );
+
+		$args = array(
+			'numberposts'      => $a['quantity'],
+			'offset'           => 0,
+			'category'         => 0,
+			'orderby'          => 'post_date',
+			'order'            => 'DESC',
+			'post_type'        => 'post',
+			'post_status'      => 'draft, publish, future, pending, private',
+			'suppress_filters' => true
+		);
+
+		$recent_posts = wp_get_recent_posts( $args, ARRAY_A );
+		$slides = '';
+
+		foreach ( $recent_posts as $p ) {
+			$url     = get_permalink( $p );
+			$thumbnailDiv = make_thumbnail_div( $p, $url );
+			$bodyDiv = make_body_div( $p, $url );
+			$slide = '<li><div class="media">' . $thumbnailDiv . $bodyDiv . '</div></li>';
+			$slides .= $slide;
+		}
+
+		return "<div class='recentPostsSlider'><ul>$slides</ul></div>";
+	}
+
+	function make_body_div( $p, $url ) {
+		$title   = $p['post_title'];
+		$excerpt = get_excerpt_by_id( $p['ID'] );
+		$h4      = "<h4 class='media-heading'><a href='$url'>$title</a></h4>";
+		$text    = "<p>$excerpt</p>";
+		$bodyDiv = "<div class='media-body'>{$h4}{$text}</div>";
+
+		return $bodyDiv;
+	}
+
+	function make_thumbnail_div( $p, $url ) {
+		$image        = get_the_post_thumbnail( get_post( $p['ID'] ), 'thumbnail', array(
+			'class' => 'media-object'
+		) );
+
+		return ($image) ? "<div class='media-left'><a href='$url' class='thumbnail'>$image</a></div>" : '';
+	}
+
+	function get_excerpt_by_id($post_id){
+		$the_post = get_post($post_id); //Gets post ID
+		$the_excerpt = $the_post->post_content; //Gets post_content to be used as a basis for the excerpt
+		$excerpt_length = 35; //Sets excerpt length by word count
+		$the_excerpt = strip_tags(strip_shortcodes($the_excerpt)); //Strips tags and images
+		$words = explode(' ', $the_excerpt, $excerpt_length + 1);
+		if(count($words) > $excerpt_length) :
+			array_pop($words);
+			array_push($words, '…');
+			$the_excerpt = implode(' ', $words);
+		endif;
+		$the_excerpt = '<p>' . $the_excerpt . '</p>';
+		return $the_excerpt;
 	}
 
 	add_shortcode( 'bootstrap_slider', 'bootstrap_slider_space\\slider' );
